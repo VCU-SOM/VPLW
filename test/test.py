32cdcb97925e3151719087e9cda2a1db2e4c3cf6
ssnegus <98348197+ssnegus@users.noreply.github.com>
2022-01-25 14:05:12 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,7 +1,7 @@
 ## Parameters
 
 F = 1
-Rt = 99
+Rt = 100
 E = 1
 Kd = 1
 threshold = 0
@@ -10,7 +10,7 @@ capacity = 100
 ## Parameters
 
 ex2_F = 1
-ex2_Rt = 80
+ex2_Rt = 66
 ex2_E = 1
 ex2_Kd = 1
 ex2_threshold = 0
@@ -19,7 +19,7 @@ ex2_capacity = 100
 ## Parameters
 
 ex3_F = 1
-ex3_Rt = 60
+ex3_Rt = 33
 ex3_E = 1
 ex3_Kd = 1
 ex3_threshold = 0
@@ -28,7 +28,7 @@ ex3_capacity = 100
 ## Parameters
 
 ex4_F = 1
-ex4_Rt = 40
+ex4_Rt = 0
 ex4_E = 1
 ex4_Kd = 1
 ex4_threshold = 0
