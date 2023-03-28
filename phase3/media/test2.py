68fc900cccd969731102102e8ae344722ece4ac3
ssnegus <98348197+ssnegus@users.noreply.github.com>
2023-03-28 16:36:52 -0400

    Update test2.py

--- phase3/media/test2.py
+++ phase3/media/test2.py
@@ -7,15 +7,15 @@ Rt = 100
 
 ## Drug Parameters
 
-Ea = 0
-Ka = 0.1
-Eb = 1
-Kb = 2
+Ea = 1
+Ka = 0.2
+Eb = 0
+Kb = 1
 
 ## Assay Parameters
 
-threshold = 30
-capacity = 90
+threshold = 20
+capacity = 60
 
 ## Ex2 
 
@@ -26,15 +26,15 @@ ex2_Rt = 100
 
 ## Drug Parameters
 
-ex2_Ea = 0
-ex2_Ka = .1
-ex2_Eb = 1
-ex2_Kb = 2
+ex2_Ea = 1
+ex2_Ka = .2
+ex2_Eb = 0
+ex2_Kb = 1
 
 ## Assay Parameters
 
-ex2_threshold =30
-ex2_capacity = 90
+ex2_threshold =20
+ex2_capacity = 60
 
 ## Ex3
 
@@ -45,15 +45,15 @@ ex3_Rt = 100
 
 ## Drug Parameters
 
-ex3_Ea = 0
-ex3_Ka = .1
-ex3_Eb = 1
-ex3_Kb = 2
+ex3_Ea = 1
+ex3_Ka = .2
+ex3_Eb = 0
+ex3_Kb = 1
 
 ## Assay Parameters
 
-ex3_threshold = 30
-ex3_capacity = 90
+ex3_threshold = 20
+ex3_capacity = 60
 
 ## Ex4
 
@@ -64,12 +64,12 @@ ex4_Rt = 100
 
 ## Drug Parameters
 
-ex4_Ea = 0
-ex4_Ka = .1
-ex4_Eb = 1
-ex4_Kb = 2
+ex4_Ea = 1
+ex4_Ka = .2
+ex4_Eb = 0
+ex4_Kb = 1
 
 ## Assay Parameters
 
-ex4_threshold = 30
-ex4_capacity = 90
+ex4_threshold = 20
+ex4_capacity = 60
