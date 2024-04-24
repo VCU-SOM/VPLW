fca9fe742f5a4070da07bc3934f13951fcd113e2
ssnegus <98348197+ssnegus@users.noreply.github.com>
2024-04-24 14:07:05 -0400

    Update test.py

--- phase3/media/test.py
+++ phase3/media/test.py
@@ -2,34 +2,34 @@
 
 F = 1
 Rt = 100
-E = 0.9
-Kd = 3
+E = 1
+Kd = 0.1
 threshold = 5
-capacity = 30
+capacity = 25
 
 ## Parameters
 
 ex2_F = 1
-ex2_Rt = 110
-ex2_E = 0.9
+ex2_Rt = 100
+ex2_E = 0.3
 ex2_Kd = 3
 ex2_threshold = 5  
-ex2_capacity = 30
+ex2_capacity = 25
 
 ## Parameters
 
-ex3_F = 0.5
-ex3_Rt = 80
-ex3_E = 0.9
-ex3_Kd = 3
+ex3_F = 1
+ex3_Rt = 100
+ex3_E = 1
+ex3_Kd = 10
 ex3_threshold = 5
-ex3_capacity = 30
+ex3_capacity = 25
 
 ## Parameters
 
-ex4_F = 0.4
-ex4_Rt = 75
-ex4_E = 0.9
-ex4_Kd = 3
+ex4_F = 1
+ex4_Rt = 100
+ex4_E = 0.3
+ex4_Kd = 10
 ex4_threshold = 5
-ex4_capacity = 30
+ex4_capacity = 25
