2b56c520380ff6d5168062b6e633add69483fa22
ssnegus <98348197+ssnegus@users.noreply.github.com>
2026-02-25 10:30:01 -0500

    Update test.py

--- phase3/media/test.py
+++ phase3/media/test.py
@@ -11,7 +11,7 @@ capacity = 60
 
 ex2_F = 1
 ex2_Rt = 100
-ex2_E = 1
+ex2_E = 0.7
 ex2_Kd = 1
 ex2_threshold = 10  
 ex2_capacity = 40
@@ -20,7 +20,7 @@ ex2_capacity = 40
 
 ex3_F = 1
 ex3_Rt = 100
-ex3_E = 0.4
+ex3_E = 0.7
 ex3_Kd = 1
 ex3_threshold = 50
 ex3_capacity = 80
@@ -29,7 +29,7 @@ ex3_capacity = 80
 
 ex4_F = 1
 ex4_Rt = 100
-ex4_E = 0.1
+ex4_E = 0.7
 ex4_Kd = 1
 ex4_threshold = 10
 ex4_capacity = 90
